081732fca4076a1f35c6d57038b163a9d3a6c4f7
pypy8171 <32826471+pypy8171@users.noreply.github.com>
2017-11-20 00:12:21 +0900

    Add files via upload

---
+++ boss.py
@@ -0,0 +1,169 @@
+import random
+import game_framework
+import normal_stage
+from pico2d import *
+
+
+class Boss:
+
+    PIXEL_PER_METER = (10.0 / 0.3)           # 10 pixel 30 cm
+    RUN_SPEED_KMPH = 20.0                    # Km / Hour
+    RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+    RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+    RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+    TIME_PER_ACTION = 0.5
+    ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+    FRAMES_PER_ACTION = 8
+
+    image = None
+
+    LEFT_RUN, RIGHT_RUN, LEFT_STAND, RIGHT_STAND,JUMP= 1, 1, 1, 1,1
+
+    def __init__(self):
+        self.x, self.y = 600, 120
+        self.canvas_width = get_canvas_width()
+        self.canvas_height = get_canvas_height()
+        self.life_time = 0.0
+        self.total_frames = 0.0
+        self.frame=0
+        self.total_frame=0
+        self.dir = 0
+        self.up = 0
+        self.a=0
+        self.updir=0
+
+        if Boss.image == None:
+            Boss.image = load_image('ground2.png')
+
+    def set_map1(self, bg):
+        self.bg = bg
+    def set_map2(self,bg):
+        self.bg=bg
+    def set_map3(self,bg):
+        self.bg=bg
+    def set_map4(self,bg):
+        self.bg=bg
+
+
+
+    def update(self, frame_time):
+        #def clamp(minimum, x, maximum):
+        #    return max(minimum, min(x, maximum))
+        #def clamp(minimum,y,maximum):
+        #    return max(minimum,min(y,maximum))
+
+        self.life_time += frame_time
+        distance = Boss.RUN_SPEED_PPS * frame_time
+        self.frame = int(self.total_frames+1) % 3
+        self.total_frames += Boss.FRAMES_PER_ACTION * Boss.ACTION_PER_TIME * frame_time
+        self.total_frame += Boss.FRAMES_PER_ACTION * Boss.ACTION_PER_TIME*frame_time
+
+        self.x += (self.dir * distance)
+        self.y +=(self.up*distance)
+
+        self.x = clamp(0, self.x, self.bg.w)
+        self.y = clamp(-120,self.y,self.bg.h)
+
+        self.a = self.total_frame
+        print("%d" % self.a)
+
+
+
+        if self.a%8>7:
+            self.up, self.updir=-2,1
+#        elif self.updir==1 and self.y<115:
+#            self.up, self.updir=0,0
+
+        if self.y<-30:
+            self.x ,self.y = 100,600
+
+
+    def draw(self):
+        sx = self.x - self.bg.window_left
+        sy = self.y - self.bg.window_bottom
+        self.image.draw( sx, sy)
+
+    def stoppipe(self):
+        if self.up ==-2:
+            self.up =0
+
+
+        if self.x>430 and self.x<570 and self.y>65 and self.y<233:
+            if self.dir==1:
+                self.x-=2
+            elif self.dir==-1:
+                self.x+=2
+
+    def stoppipe2(self):
+        if self.up ==-2:
+            self.up =0
+        if self.x>1670 and self.x<1830 and self.y>65 and self.y<180:
+            if self.dir==1:
+                self.x-=2
+            elif self.dir==-1:
+                self.x+=2
+
+    def stop(self):
+        if self.up==-2:
+            self.up=0
+
+    def normal_stop(self):
+        if self.up==-2:
+            self.up=0
+        if self.dir==1 and self.up==0:
+            self.x-=4
+        if self.dir==-1 and self.up==0:
+            self.x+=4
+
+    def block_stop(self):
+        if self.dir==1 and self.up==2:
+            self.up=-20
+        if self.dir==-1 and self.up==2:
+            self.up=-20
+
+
+    def get_bb(self):
+        return self.x - 20-self.bg.window_left, self.y - 40-self.bg.window_bottom, self.x+25-self.bg.window_left, self.y+40-self.bg.window_bottom
+        pass
+
+    def draw_bb(self):
+        draw_rectangle(*self.get_bb())
+
+    def handle_event(self, event):
+        if (event.type, event.key) == (SDL_KEYDOWN, SDLK_LEFT):
+            if self.state in (self.RIGHT_STAND, self.LEFT_STAND, self.RIGHT_RUN,self.JUMP):
+                self.state = self.LEFT_RUN
+                self.dir = -1
+                normal_stage.a=0
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_RIGHT):
+            if self.state in (self.RIGHT_STAND, self.LEFT_STAND, self.LEFT_RUN,self.JUMP):
+                self.state = self.RIGHT_RUN
+                self.dir = 1
+                normal_stage.a=0
+        elif (event.type, event.key) == (SDL_KEYUP, SDLK_LEFT):
+            if self.state in (self.LEFT_RUN,self.JUMP):
+                self.state = self.LEFT_STAND
+                self.dir = 0
+                normal_stage.a=0
+        elif (event.type, event.key) == (SDL_KEYUP, SDLK_RIGHT):
+            if self.state in (self.RIGHT_RUN,self.JUMP):
+                self.state = self.RIGHT_STAND
+                self.dir = 0
+                normal_stage.a=0
+        elif (event.type, event.key)==(SDL_KEYDOWN,SDLK_UP):
+            if self.state in(self.RIGHT_STAND,self.RIGHT_RUN,self.LEFT_STAND, self.LEFT_RUN):
+                if self.up<1 and self.up>-1:
+                    self.state = self.JUMP
+                    self.up = 2
+                    self.total_frame=0
+
+
+
+
+
+
+
+
+
+
---
+++ boss_stage.py
@@ -0,0 +1,161 @@
+from pico2d import *
+
+import game_framework
+import pause_state
+
+import title_state
+import normal_stage
+import hard_stage
+
+
+from cat import Cat # import Boy class from boy.py
+from land4 import Land4
+from pistol import Pistol
+from map4 import Map4
+from boss import Boss
+
+
+name = "easy_stage"
+
+cat = None
+map1 = None
+pipe = None
+land = None
+pistol=None
+
+def create_land():
+    land = []
+    for i in range(0, 22):
+        ground = Land4()
+        ground.x =20+40*i
+        ground.y = 40
+        land.append(ground)
+
+
+    return land
+
+
+
+def create_world():
+    global cat,map4,land, wall,door, pistol,boss
+
+    boss = Boss()
+    cat = Cat()
+    map4=Map4()
+    land=Land4()
+    wall = create_land()
+    pistol = Pistol()
+
+
+    for i in wall:
+        i.set_map4(map4)
+    map4.set_center_object(cat)
+    cat.set_map4(map4)
+    land.set_map4(map4)
+    pistol.set_map4(map4)
+    boss.set_map4(map4)
+
+def destroy_world():
+    global cat,map4,land,pistol,boss
+
+    del(boss)
+    del(cat)
+    del(map4)
+    del(land)
+    del(pistol)
+
+
+
+
+
+def enter():
+    open_canvas()
+    game_framework.reset_time()
+    create_world()
+
+
+def exit():
+    destroy_world()
+    close_canvas()
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
+
+
+def handle_events(frame_time):
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        else:
+            if (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
+                game_framework.quit()
+            else:
+                cat.handle_event(event)
+                map4.handle_event(event)
+                pistol.handle_event(event)
+                #land.handle_event(event)
+
+
+def collide(a, b):
+    left_a, bottom_a, right_a, top_a = a.get_bb()
+    left_b, bottom_b, right_b, top_b = b.get_bb()
+
+    if left_a>right_b:return False
+    if right_a < left_b:return False
+    if top_a<bottom_b:return False
+    if bottom_a>top_b:return False
+
+    return True
+
+
+def update(frame_time):
+
+    cat.update(frame_time)
+    map4.update(frame_time)
+    land.update(frame_time)
+    pistol.update(frame_time)
+    boss.update(frame_time)
+
+    for ground in wall:
+        if collide(ground,boss):
+            boss.stop()
+    for ground in wall:
+        if collide(ground,cat):
+            cat.stop()
+        elif collide(ground,pistol):
+            pistol.stop()
+
+
+def draw(frame_time):
+    clear_canvas()
+
+    boss.draw()
+    pistol.draw()
+    map4.draw()
+    cat.draw()
+    land.draw()
+    for ground in wall:
+        ground.draw()
+
+    pistol.draw_bb()
+    map4.draw_bb()
+    cat.draw_bb()
+    land.draw_bb()
+    boss.draw_bb()
+    for ground in wall:
+        ground.draw_bb()
+    pass
+
+    update_canvas()
+
+
+
+
+
+
